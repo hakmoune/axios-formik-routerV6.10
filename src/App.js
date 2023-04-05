23aecee627b2a46ad1349954cb70357113a9967f
mehdi <mehdi.hakmoune@gmail.com>
2023-04-05 05:49:36 +0000

    Test axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then(res => setProducts(res.data.products))
+      .catch(error => console.log(error.message));
+  }, []);
+
+  //Async and Await
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    try {
+      let res = await axios.post("https://dummyjson.com/products/add", {
+        title: formData.get("title")
+      });
+      setProducts([res.data, ...products]);
+    } catch (err) {
+      console.log(err.message);
+    }
+
+    form.reset();
+    form.querySelector("input").focus();
+  };
+
+  //Promise
+  /*const handleSubmit = async e => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    axios
+      .post("https://dummyjson.com/products/add", {
+        title: formData.get("title")
+      })
+      .then(res => setProducts([res.data, ...products]))
+      .catch(err => console.log(err.message));
+
+    form.reset();
+    form.querySelector("input").focus();
+  };*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List products with their posts</h1>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="title" />
+          <button type="submit">Ajouter un produit</button>
+        </form>
+      </div>
+
+      <ul>
+        {products.map(product => (
+          <DisplayUser product={product} key={product.id} />
+        ))}
+      </ul>
     </div>
   );
+}
+
+function DisplayUser({ product }) {
+  return <li>{product.title}</li>;
 }
 
 export default App;
